8c4f3dd9182ecbcc022a41fe75a207a7951c9d7b
qisyuu <22001887@siswa.um.edu.my>
2025-06-16 00:56:47 +0800

    1

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -102,7 +102,7 @@ router.put('/deactivate/:id', async (req, res) => {
 // 🔁 Recover account
 router.put('/recover/:id', async (req, res) => {
   try {
-    const user = await User.findOneAndUpdate(
+    const user = await User.findByIdAndUpdate(
       { id: req.params.id },
       { isActive: true }, // Reactivate the user
       { new: true }
